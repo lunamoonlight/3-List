5fc985cd7f2f0719e23d0324a1b83c0405ecd127
lunamoonlight <ksusha270901@gmail.com>
2025-09-26 11:12:27 +0500

    Update script.js

--- Новая папка/script.js
+++ Новая папка/script.js
@@ -1,17 +1,27 @@
-let inputOne=document.getElementById("one")
-console.log(inputOne.value);
-let inputTwo=document.getElementById("two")
-console.log(inputTwo.value);
-let buttonSum=document.getElementById("sum")
-console.log(buttonSum.value);
+const inputOne=document.getElementById("one")
+const inputTwo=document.getElementById("two")
+const buttonSum=document.getElementById("sum")
+const output=document.getElementById("output")
 
+sum.addEventListener('click',(e)=> {
+    e.preventDefault()
+    const numberOrNan1=Number(inputOne.value)
+    const numberOrNan2=Number(inputTwo.value)
 
-if (typeof Number.isFinite(inputOne.value) && typeof Number.isFinite(inputTwo.value)) {
-    let buttonSum=inputOne.value+inputTwo.value;
-    console.log(buttonSum.value);
-}
-else {
-    console.log("Некорректное значение");
-}
+    let result=''
 
+    if (isNaN(numberOrNan1) && isNaN(numberOrNan2)){
+        result= "Некорректное значение в обоих полях"
+    }
+    if (!isNaN(numberOrNan1) && isNaN(numberOrNan2)){
+        result= "Некорректное значение во 2 поле"
+    }
+    if (isNaN(numberOrNan1) && !isNaN(numberOrNan2)){
+        result= "Некорректное значение в 1 поле"
+    }
+    if (!isNaN(numberOrNan1) && !isNaN(numberOrNan2)){
+        result= `Результат равен  ${numberOrNan1+numberOrNan2}`
+    }
+    output.innerText = result
+})
 
